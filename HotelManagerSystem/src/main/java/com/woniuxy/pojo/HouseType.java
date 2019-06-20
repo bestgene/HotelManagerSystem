5c818bd5490f2241e1272b5d4a1310559843dabd
tony131143a <981788484@qq.com>
2019-06-20 16:32:30 +0800

    整合

--- HotelManagerSystem/src/main/java/com/woniuxy/pojo/HouseType.java
+++ HotelManagerSystem/src/main/java/com/woniuxy/pojo/HouseType.java
@@ -18,53 +18,6 @@ public class HouseType implements Serializable{
 	//房间头像
 	private Integer flag;
 	//软删除
-<<<<<<< HEAD
-	public HouseType(Integer house_type_id, String house_type_name, BigDecimal house_type_price, String house_type_img,
-			Integer flag) {
-		this.house_type_id = house_type_id;
-		this.house_type_name = house_type_name;
-		this.house_type_price = house_type_price;
-		this.house_type_img = house_type_img;
-		this.flag = flag;
-	}
-	public HouseType() {}
-	@Override
-	public String toString() {
-		return "HouseType [house_type_id=" + house_type_id + ", house_type_name=" + house_type_name
-				+ ", house_type_price=" + house_type_price + ", house_type_img=" + house_type_img + ", flag=" + flag
-				+ "]";
-	}
-	public Integer getHouse_type_id() {
-		return house_type_id;
-	}
-	public void setHouse_type_id(Integer house_type_id) {
-		this.house_type_id = house_type_id;
-	}
-	public String getHouse_type_name() {
-		return house_type_name;
-	}
-	public void setHouse_type_name(String house_type_name) {
-		this.house_type_name = house_type_name;
-	}
-	public BigDecimal getHouse_type_price() {
-		return house_type_price;
-	}
-	public void setHouse_type_price(BigDecimal house_type_price) {
-		this.house_type_price = house_type_price;
-	}
-	public String getHouse_type_img() {
-		return house_type_img;
-	}
-	public void setHouse_type_img(String house_type_img) {
-		this.house_type_img = house_type_img;
-	}
-	public Integer getFlag() {
-		return flag;
-	}
-	public void setFlag(Integer flag) {
-		this.flag = flag;
-	}
-=======
 	private Integer num;
 	//相同类型的房间数量
 	public HouseType(Integer house_type_id, String house_type_name, BigDecimal house_type_price, String house_type_img,
@@ -121,5 +74,4 @@ public class HouseType implements Serializable{
 				+ ", num=" + num + "]";
 	}
 	
->>>>>>> refs/remotes/origin/Tony
 }
