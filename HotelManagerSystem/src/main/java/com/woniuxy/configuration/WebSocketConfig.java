bf0a3aec9daa0c4e7c9073f6a480cab8e89c0dbe
44944608ggg <1203495308@qq.com>
2019-06-20 20:06:25 +0800

    二级缓存

---
+++ HotelManagerSystem/src/main/java/com/woniuxy/configuration/WebSocketConfig.java
@@ -0,0 +1,13 @@
+package com.woniuxy.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebSocketConfig {
+
+	/*@Bean
+	public ServerEndpointExporter serverEndpointExporter() {
+		return new ServerEndpointExporter();
+	}*/
+}
